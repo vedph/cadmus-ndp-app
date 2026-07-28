276acc6d8987f1182538317fd4e68b21f9eb2ee4
Myrmex <dfusi@hotmail.com>
2026-07-28 09:25:52 +0200

    Upgraded to Maplibre v6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # History
 
+- 2026-07-28: upgraded to Maplibre v6.
 - 2026-07-01: updated packages.
 - 2026-06-25:
   - updated Angular and packages.
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { setWorkerUrl } from 'maplibre-gl';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+setWorkerUrl(new URL('assets/maplibre-gl/maplibre-gl-worker.mjs', document.baseURI).toString());
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
